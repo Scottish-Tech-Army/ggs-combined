bb0fec20bd4f9b0809abe0bc355f46cfd8e04d61
Mukund Pandit <mukund.pandit@virginmedia.com>
2023-06-10 08:24:46 +0100

    Fix for problem in issue gga-126 (and gga-114) plus minor changes to in-code comments.

--- ggs-frontend/src/components/LocationModal.js
+++ ggs-frontend/src/components/LocationModal.js
@@ -40,11 +40,28 @@ const LocationModal = ({
   const [isOutOfRange, setIsOutOfRange] = useState(true);
   const [deviceErrMsg, setDeviceErrMsg] = useState();
 
+
+// A state property that is a boolean. Its value determines
+// whether or not the "Congratulations! ..." message will 
+// appear in this modal. When the user clicks the button
+// that allows her to collect a location the click handler
+// for that button sets this state property to true,
+// causing conditional rendering of the "Congratulations! ..."
+// message:
+  const [showCongrats, setShowCongrats] = useState(false);
+
   const handleCollectLocation = (event) => {
+    // The following function tells the backend that the 
+    // user has collected the location of the given id:
     collectLocation(unit.email, selectedLocation.locationId)
       .then((response) => {
         if (response.ok) {
-          // Update the frontend locations with the collected marker to match the backend status
+          // Update the frontend locations with the 
+          // collected marker to match the backend status.
+          // The following function simply sets to true the 
+          // collected property of the object (a member of 
+          // array locations) that corresponds to
+          // the location in question: 
           setLocations((locations) => {
             const index = locations.findIndex(
               (i) => i.locationId === selectedLocation.locationId
@@ -53,7 +70,11 @@ const LocationModal = ({
             locationList[index].collected = true;
             return locationList;
           });
-        }
+          // Also set state property showCongrats to true
+          // so that the "Congratulations! ..." message 
+          // shows:
+          setShowCongrats(true)
+                         }
       })
       .catch((error) => {
         console.error(error);
@@ -61,27 +82,52 @@ const LocationModal = ({
     event.preventDefault();
   };
 
-  // Mukund: Set a variable to the object that is in array photos 
-  // of object selectedLocation. array photos looks like this
+// set a variable to the jsx for the "Congratulations! ... " text:
+let congratsMessage = (   
+<> 
+<h1>Congratulations!</h1>
+  <div>
+    <p>You have collected this location:</p>
+  </div>
+</>
+                   )
+
+
+
+  // Set a variable to the object that is in array photos, which is a property of 
+  // object selectedLocation. array photos looks like this
   // (it contains only one object):
   // [{url:  "https://dz1ex1yb0vxyz.cloudfront.net/dunbartonshire-bearsden-thegruffalotrail.jpg"}]
     const photo = selectedLocation.photos[0];
 
+
+
+// The following useEffect either 
+// 1) if location tracking is not on:
+//    sets the error message, 
+//    sets boolean state property isOutOfRange to true
+// 2) if location tracking IS on:
+//    finds out whether the user is in range or out of range
+//    and sets boolean state property isOutOfRange accordingly.
+// The useEffect runs whenever the user moves (which changes 
+// userLatLong) or whenever the user clicks on a new marker
+// (which changes selectedLocation)
   useEffect(() => {
-    if (!userLatLong) {
+    if (!userLatLong) { // 1):
       setDeviceErrMsg("Please turn on location tracking");
       setIsOutOfRange(true);
-    } else {
+    } else {  // 2):
       setIsOutOfRange(!isLocationInRange(selectedLocation, userLatLong));
     }
   }, [selectedLocation, userLatLong]);
 
+  
+  
   // Adapt the collecting button depending on user's proximity to the landmark location
-  // and the collection status of the landmark location
+  // and the collection status of the landmark location.
+  // selectedLocation.collected and isOutOfRange are booleans.
   const collectButtonDisabled = selectedLocation.collected || isOutOfRange;
-  const collectButtonText = isOutOfRange
-    ? "Please come closer to this location"
-    : "Tap to reveal challenge";
+  const collectButtonText = isOutOfRange ? "Please come closer to this location" : "Tap to reveal challenge";
 
   const areaName =
     selectedLocation.city && selectedLocation.city !== selectedLocation.county
@@ -95,10 +141,6 @@ const LocationModal = ({
 // If there's no photo available shove this into the
 // <Modal.Body/>:
         <>
-        <h1>Congratulations!</h1>
-        <div>
-          <p>You have collected this location:</p>
-        </div>
           <Image
             className="img-location"
             src={placeholderPhoto}
@@ -136,15 +178,11 @@ const LocationModal = ({
       );
     }
 
-    return (
-// If there IS a photo available shove this into the
-// <Modal.Body/>:
-      <>
-      <h1>Congratulations!</h1>
-        <div>
-          <p>You have collected this location:</p>
-        </div>
 
+    // If there IS a photo available shove this into the
+// <Modal.Body/>:
+    return (
+      <>
         <Image
           className="img-location"
           src={photo.url}
@@ -153,7 +191,7 @@ const LocationModal = ({
         />
         {creditLine}
       </>
-    );
+          );
                         } // end getPhoto
 
 
@@ -167,6 +205,8 @@ const LocationModal = ({
       className="custom-modal location-modal"
     >
       <Modal.Header className="border-0 mb-n4">
+
+        {/*The button the user clicks to close the modal. It has an X image in it: */} 
         <Button
           variant="outline-primary"
           onClick={handleCloseLocation}
@@ -180,7 +220,11 @@ const LocationModal = ({
           />
         </Button>
       </Modal.Header>
+
       <Modal.Body className="mt-n3">
+        {/*If the user has clicked the button that reads "Tap to reveal challenge"
+        show the "Congratulations! ... " message: */} 
+      {showCongrats ? congratsMessage : null} 
         <div className="place-name">{selectedLocation.name}</div>
         <div className="city-name">{areaName}</div>
         <div className="scroll-container">
@@ -196,15 +240,25 @@ const LocationModal = ({
           </div>
         </div>
       </Modal.Body>
+      {/*If there's an error show the error message: */}
       {!!deviceErrMsg && (
         <div className="container">
           <img src={dividerLine} style={{ width: "100%" }} alt="" />
           <p className="feedback-branding">{deviceErrMsg}</p>
         </div>
       )}
+      {/*If there's no error and the user has NOT collected 
+      the location, show the button that allows the user to 
+      collect the location: */}
       {!selectedLocation.collected && !deviceErrMsg && (
         <Button
           bsPrefix="btn-branding"
+          // The following function tells the backend 
+          // to mark the location in question as
+          // collected. It also gets the object in 
+          // array locations (here in the frontend) 
+          // that corresponds to the location and sets 
+          // its collected property to true:
           onClick={handleCollectLocation}
           disabled={collectButtonDisabled}
           className={
